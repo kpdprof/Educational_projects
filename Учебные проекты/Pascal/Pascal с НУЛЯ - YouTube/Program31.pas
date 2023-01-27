afbddccdcb4a2ebc7e7beada0056daa1f3df6460
Roman Sechenov <kpdprof@gmail.com>
2023-01-27 11:01:12 +0700

    1

--- Учебные проекты/Pascal/Pascal с НУЛЯ - YouTube/Program31.pas
+++ Учебные проекты/Pascal/Pascal с НУЛЯ - YouTube/Program31.pas
@@ -1,11 +1,13 @@
 ﻿program Task_31;
-var n, i, f: integer;
+var i, x1, x2, x3, sum: integer;
 begin
-  f := 1; 
-  readln(n);
-  for i:=1 to n do
+  for i:=100 to 999 do
     begin
-      f := f*i ;
-      writeln('Шаг ', i, '. ',f);
+      x1 := i div 100;
+      x2 := (i div 10) mod 10;
+      x3 := i mod 10;
+      sum := (x1*x1*x1) + (x2*x2*x2) + (x3*x3*x3);
+      if sum = i then
+        writeln(i);
     end;
 end.
